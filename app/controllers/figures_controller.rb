bbc965e13bf860010ed134324660ddbb7b6a9e3a
John Pfingst <jpfingst@outlook.com>
2017-03-02 02:37:24 +0000

    /figures/new tests passed

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,12 +5,23 @@ class FiguresController < ApplicationController
   end
 
   post '/figures/new' do
-    figure = Figure.create(name: params[:figure][:name])
-    binding.pry
+    figure = Figure.new(name: params[:figure][:name])
+
+    if params[:figure][:title_ids]
+      figure.titles = params[:figure][:title_ids].map { |title_id| Title.find title_id }
+    end
     if params[:title][:name].length > 0
       figure.titles << Title.create(name: params[:title][:name])
-#      figure.titles = params['genres'].map { |genre_id| Genre.find genre_id }
     end
+
+    if params[:figure][:landmark_ids]
+      figure.landmarks = params[:figure][:landmark_ids].map { |landmark_id| Landmark.find landmark_id }
+    end
+    if params[:landmark][:name].length > 0
+      figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+
+    figure.save
   end
 
 end
